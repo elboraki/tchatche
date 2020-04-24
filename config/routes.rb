57a39361d14faaa805d971194c43e7f8ddd6f43a
elboraki <younes.elboraki@gmail.com>
2020-04-24 11:11:02 +0000

    create chatroom channel

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   post 'message', to: 'messages#create'
 
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
